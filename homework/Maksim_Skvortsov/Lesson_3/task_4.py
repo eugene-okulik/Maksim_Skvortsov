8fa0bf9b3c363e8e627bac66bd2da536d9d43411
Maksim Skvortsov <mskvortsov@inno.tech>
2026-04-22 00:08:39 +0300

    Исправил пробелы

--- homework/Maksim_Skvortsov/Lesson_3/task_4.py
+++ homework/Maksim_Skvortsov/Lesson_3/task_4.py
@@ -1,6 +1,6 @@
 a = int(input("Введите первое число "))
 b = int(input("Введите второе число "))
-result1 =  (a * b) / 2
-result2 = ((a ** 2) + (b ** 2)) ** (1/2)
+result1 = (a * b) / 2
+result2 = ((a ** 2) + (b ** 2)) ** (1 / 2)
 print("Площадь =", int(result1))
 print("Гипотенуза =", int(result2))
